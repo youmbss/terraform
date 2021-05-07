125611bd4725286d5cae1ba1e91d4d069dce38b1
arnauld youmbi <arnauldy@yahoo.com>
2021-05-07 13:38:39 -0400

    adding this the github

---
+++ instance.tf
@@ -0,0 +1,24 @@
+provider "aws" { 
+   region = "ca-central-1"
+  
+}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {} 
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+      Name: "${var.env_prefix}-vpc"
+    }
+}
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+      Name: "${var.env_prefix}-subnet-1"
+    }
+  
+}
